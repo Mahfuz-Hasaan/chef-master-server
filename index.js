56a33702a5a8cdb86636050757db62924b054390
Mahfuz-Hasan <mahfuzhasan587@gmail.com>
2023-05-03 21:11:40 +0600

    chef_details data loaded

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
 const cors = require('cors')
-const chef = require('./data/shef_data.json')
+const chef = require('./data/shef_data.json');
+const chef_details = require('./data/shef_details.json');
 
 app.use(cors());
 
@@ -13,6 +14,15 @@ app.get("/", (req, res) => {
 app.get("/chef", (req, res) => {
   res.send(chef);
 });
+
+app.get("/chef/:id", (req,res) => {
+    const id = req.params.id;
+    console.log(id);
+    const chefDetails = chef_details.find(n => n.chef_id === id);
+    res.send(chefDetails);
+
+
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
