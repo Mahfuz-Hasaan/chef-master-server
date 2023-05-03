068e7a63eab6625e900527d7665e2f72a7e840cb
Mahfuz-Hasan <mahfuzhasan587@gmail.com>
2023-05-03 16:06:31 +0600

    chef-data modified

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
+const cors = require('cors')
+const chef = require('./data/shef_data.json')
+
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Master chef server is running!");
+});
+
+app.get("/chef", (req, res) => {
+  res.send(chef);
 });
 
 app.listen(port, () => {
